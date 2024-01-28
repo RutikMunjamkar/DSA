8d3b4588c6971c8e87f5619e40c0a42aef573fe0
RutikMunjamkar <138121126+RutikMunjamkar@users.noreply.github.com>
2024-01-28 07:36:52 +0530

    Update Course_Schdeule.java

--- Graphs/TopoLogical Sort/Course_Schdeule.java
+++ Graphs/TopoLogical Sort/Course_Schdeule.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public int[] findOrder(int n, int[][] arr) {
         HashMap<Integer,List<Integer>>hash=new HashMap<>();
@@ -21,19 +20,30 @@ class Solution {
                 topo(i,hash,stack,vis);
             }
         }
-    }
-    public static int topo(int i, HashMap<Integer,List<Integer>>hash, Stack<Integer>stack, int[]vis){
-        vis[i]=1;
-        for(int j=0;<hash.get(i).size();j++){
-            int dest=hash.get(i).get(j);
+        int i=0;int[]res=new int[n];
+        while(!stack.isEmpty()){
+            int x=stack.pop();
+            res[i]=x;
+            i++;
         }
+        return res;
+    }
+    public static void topo(int i, HashMap<Integer,List<Integer>>hash, Stack<Integer>stack, int[]vis){
+        vis[i]=1;
+        for(int j=0;j<hash.get(i).size();j++){
+            int dest=hash.get(i).get(j);
+            if(vis[dest]==0){
+                topo(dest,hash,stack,vis);
+            }
+        }
+        stack.push(i);
     }
     public static boolean findCycle(int n,HashMap<Integer,List<Integer>>hash){
         int[]vis=new int[n];
         int[]pathvis=new int[n];
-        for(int 0;i<n;i++){
+        for(int i=0;i<n;i++){
             if(vis[i]==0){
-                if(i, detectCycle(hash,vis,pathvis)==true){
+                if(detectCycle(i, hash,vis,pathvis)==true){
                     return true;
                 }
             }
@@ -51,7 +61,8 @@ class Solution {
                 }
             }
             else{
-                if(pathvis[i]==1){
+                if(pathvis[dest]==1){
+                    b=true;
                     break;
                 }
             }
